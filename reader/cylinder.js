55d50440655550c5b96ea382260b906be643982a
João Cabral <up201304395@fe.up.pt>
2015-09-30 18:34:10 +0000

    Imported cylinder from 2nd semester CGRA project, and changed the drawing algorithm to take into account the new cylinder properties to be supported on the lsx file.

--- reader/cylinder.js
+++ reader/cylinder.js
@@ -5,14 +5,14 @@
  /*
  * I have assumed the origin of the cylinder to be the center and it will grow up (y+)
  */
- function cylinder(scene, height, br, tr, stacks, slices) {
+ function cylinder(scene, height, br, tr, stacks, slices) { //4, 2, 0.5, 4, 8 -testCyllinder
  	CGFobject.call(this,scene);
 	
 	this.height = height;
 	this.br = br;
 	this.tr = tr;
 	this.slices=slices;
-	this.stacks=stacks;//+1;
+	this.stacks=stacks;
 	
  	this.initBuffers();
  };
@@ -21,41 +21,32 @@
  cylinder.prototype.constructor = cylinder;
 
  cylinder.prototype.initBuffers = function() {
- 	
 
- 	this.vertices = [];
+	this.vertices = [];
  	this.indices = [];
  	this.normals = [];
  	var ang = 2*Math.PI/this.slices;
 
 
-	for(var i = 0; i < this.stacks; i++){
-		for(var j = 0; j < this.slices; j++){
-			var x = Math.cos(ang*j); //falta botar os raios d cima e d baixo
-			var z = Math.sin(ang*j); 
-			var y = i/this.height;
+ 	for(var i = 0; i < this.slices; i++){
+ 		for(var j = 0; j < this.stacks; j++){
+ 			var radius = this.br + ((this.tr - this.br)/this.stacks)*j;
+ 			var x = radius*Math.cos(ang*i);
+ 			var y = radius*Math.sin(ang*i);
+ 			this.vertices.push(x, y, j*(this.height/this.stacks));
 
-			this.vertices.push(x, y, z);
-			this.normals.push(x, 0, z);
+ 			this.normals.push(x, y, 0);
+ 		}
+ 	}
+ 
+	for(var i = 0;i < this.stacks-1;i++){
+		for(var j = 0; j < this.slices-1;j++){
+			this.indices.push((j+1)*this.stacks+i, j*this.stacks + (i+1), j*this.stacks+i);
+			this.indices.push((j+1)*this.stacks+i, (j+1)*this.stacks+(i+1),  j*this.stacks + (i+1));
 		}
+		this.indices.push((this.stacks*this.slices - this.stacks)+(i+1), (this.stacks*this.slices - this.stacks)+i , i);
+		this.indices.push(i, i+1, (this.stacks*this.slices - this.stacks)+(i+1));
 	}
-
-	for(var i = 0; i < this.stacks-1; i++){
-		for(var j = 0; j < this.slices-1; j++){
-			this.indices.push((i*this.slices) + this.slices+j, (i*this.slices) +j+1, (i*this.slices) +j);
-			this.indices.push((i*this.slices) +j+1, (i*this.slices) +this.slices+j, (i*this.slices) +this.slices+j+1);
-		}
-		this.indices.push((i*this.slices) +this.slices+j, (i*this.slices) +0, (i*this.slices) +j);
-		//falta aqui cenas
-
-	}
-
-
-
-
-	console.log(this.indices.length/3);
-	console.log(this.indices);
-	console.log(this.vertices.length/3);
 
 
 	this.primitiveType = this.scene.gl.TRIANGLES;
